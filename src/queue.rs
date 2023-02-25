a641213bace3c72df66ecb7e0f01ce0805e39c98
GV Aditya <gvaditya@gvs-macbook-pro.speedport.ip>
2023-02-25 14:39:18 +0100

    fix(queue): fixed ds from stack to queue

--- src/queue.rs
+++ src/queue.rs
@@ -1,3 +1,5 @@
+use std::ptr;
+
 type Link<T> = Option<Box<Node<T>>>;
 
 #[derive(Debug)]
@@ -16,6 +18,7 @@ impl<T> Node<T> {
 pub struct Queue<T> {
     length: i32,
     head: Link<T>,
+    tail: *mut Node<T>,
 }
 
 impl<T> Queue<T> {
@@ -23,25 +26,25 @@ impl<T> Queue<T> {
         Self {
             length: 0,
             head: None,
+            tail: ptr::null_mut(),
         }
     }
 
     fn enqueue(&mut self, item: T) {
         // create a new node
         let mut new_node = Box::new(Node::new(item));
-        // get the node in head
-        let current_node = self.head.take();
+        let raw_tail: *mut _ = &mut *new_node;
 
-        match current_node {
-            Some(current_node) => {
-                // point the new node's next to the current node
-                new_node.next = Some(current_node);
-                // point the head to the new node
-                self.head = Some(new_node);
+        if self.tail.is_null() {
+            self.head = Some(new_node);
+            self.tail = raw_tail;
+        } else {
+            // set next of tail to the new node
+            unsafe {
+                (*self.tail).next = Some(new_node);
             }
-            None => {
-                self.head = Some(new_node);
-            }
+            // set new node as the new tail of queue
+            self.tail = raw_tail;
         }
 
         self.length += 1;
@@ -50,18 +53,26 @@ impl<T> Queue<T> {
     fn dequeue(&mut self) {
         let current_node = self.head.take();
         match current_node {
-           Some(mut current_node) => {
-               // take the next node from head
+            Some(mut current_node) => {
+                // take the next node from head
                 let next_node = current_node.next.take();
                 // point the head to the next node
                 self.head = next_node;
-           }
-           None => {
-               panic!("Nothing in the queue");
-           }
+            }
+            None => {
+                panic!("Nothing in the queue");
+            }
         }
 
         self.length -= 1;
+
+        if self.length == 0 {
+            self.tail = ptr::null_mut();
+        }
+    }
+
+    fn peek(&self) -> &Link<T> {
+        &self.head
     }
 }
 
@@ -81,6 +92,7 @@ mod tests {
 
         q.dequeue();
         assert_eq!(q.length, 1);
+        println!("Peeking at queue after dequeue {:?}", q.peek());
 
         q.dequeue();
         assert_eq!(q.length, 0);
