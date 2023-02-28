77bf11d12e438bbf1addc77532269658d95435ed
GV Aditya <gvaditya@gvs-macbook-pro.speedport.ip>
2023-02-28 12:05:09 +0100

    fix: use the correct quick sort algo

--- src/quick_sort.rs
+++ src/quick_sort.rs
@@ -1,28 +1,43 @@
-fn sort(list: Vec<i32>) -> Vec<i32> {
-    if list.len() < 2 {
-        return list;
+fn new_sort(list: &mut Vec<i32>, lo: usize, hi: usize) {
+    if lo >= hi {
+        return;
     }
 
-    let pivot = list.len() / 2;
-    let item_at_pivot = list[pivot];
+    let partition_idx = partition(list, lo, hi);
 
-    // all items smaller than the pivot go to left
-    let mut left_arr: Vec<i32> = vec![];
-    let mut right_arr: Vec<i32> = vec![];
+    let new_high = if partition_idx <= lo {
+        lo
+    } else {
+        partition_idx - 1
+    };
 
-    for (pos, e) in list.iter().enumerate() {
-        if pos != pivot {
-            if e < &item_at_pivot {
-                // push to leftArr
-                left_arr.push(e.clone());
-            } else {
-                // push to rightArr
-                right_arr.push(e.clone());
-            }
+    new_sort(list, lo, new_high);
+    new_sort(list, partition_idx + 1, hi);
+}
+
+pub fn partition(list: &mut Vec<i32>, lo: usize, hi: usize) -> usize {
+    let pivot = list[hi];
+    let mut idx = lo;
+    let mut i = lo;
+
+    loop {
+        if i > hi {
+            break;
         }
+
+        let item = list[i];
+        if item < pivot {
+            let temp = list[idx];
+            list[idx] = list[i];
+            list[i] = temp;
+            idx += 1;
+        }
+        i += 1;
     }
 
-    return vec![sort(left_arr), vec![item_at_pivot], sort(right_arr)].concat();
+    list[hi] = list[idx];
+    list[idx] = pivot;
+    idx
 }
 
 #[cfg(test)]
@@ -32,11 +47,12 @@ mod tests {
 
     #[test]
     fn test_quick_sort() {
-        let unsorted_list: Vec<i32> = vec![12, 3, 5, 6, 4, 2, 32, 9, 18, 43, 1, 8, 7];
-        let sorted_list = sort(unsorted_list);
+        let mut unsorted_list: Vec<i32> = vec![12, 3, 5, 6, 4, 2, 32, 9, 18, 43, 1, 8, 7];
+        let hi = unsorted_list.len() - 1;
+        new_sort(&mut unsorted_list, 0, hi);
 
         assert_eq!(
-            sorted_list,
+            unsorted_list,
             vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 12, 18, 32, 43]
         );
     }
