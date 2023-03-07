05ef19aee8c0eff6e58e90f3ea4e8c4859ff96fe
Aditya <androiditya@gmail.com>
2023-03-07 17:52:00 +0100

    change the delete fn

--- src/binary_tree/BST.rs
+++ src/binary_tree/BST.rs
@@ -1,3 +1,5 @@
+use std::ops::{Deref, DerefMut};
+
 use super::types::{BinaryTree, Link, Node};
 
 #[derive(Debug)]
@@ -18,58 +20,29 @@ impl BinarySearchTree {
         }
     }
 
-    pub fn delete(&mut self, item: i32) {}
-}
-
-fn walk_and_delete(node: Box<Node<i32>>, item: i32) {
-    if item < node.value {
-        if let Some(left_node) = node.left_node {
-            if left_node.value == item {
-                // find the node to replace with
-            } else {
-                walk_and_delete(left_node, item)
-            }
-        }
-    } else {
-        if let Some(right_node) = node.right_node {
-            if right_node.value == item {
-                // find the node to replace with
-            } else {
-                walk_and_delete(right_node, item)
-            }
-        }
+    pub fn delete(self, item: i32) {
+        delete_node(self.tree.root, item);
     }
 }
 
-fn replace_by_node(node: &mut Node<i32>) -> Result<&mut Box<Node<i32>>, &'static str> {
-    // if left child
-    if let Some(left_node) = node.left_node.as_mut() {
-        let mut node_to_replace = left_node;
-
-        // ...if left has a right child, get the right most child
-        let mut left_right_node = node_to_replace.right_node.as_mut();
-        while let Some(right_node) = left_right_node {
-            node_to_replace = right_node;
-            left_right_node = node_to_replace.right_node.as_mut();
-        }
-
-        return Ok(node_to_replace);
+pub fn delete_node(node: Link<i32>, item: i32) -> Link<i32> {
+    match node {
+        Some(mut node) => {
+            if item < node.value {
+                node.left_node = delete_node(node.left_node, item);
+            } else if item > node.value {
+                node.right_node = delete_node(node.right_node, item);
+            } else {
+                // if node has only one or no Child
+                if node.left_node.is_none() {
+                    let temp = node.right_node;
+                    return temp;
+                }
+            }
+            Some(node)
+        },
+        None => node,
     }
-    // else if right child
-    else if let Some(right_node) = node.right_node.as_mut() {
-        let mut node_to_replace = right_node;
-
-        // ...if right child has a left child, get the left most child
-        let mut right_left_node = right_node.left_node.as_mut();
-        while let Some(left_node) = right_left_node {
-            node_to_replace = left_node;
-            right_left_node = left_node.right_node.as_mut();
-        }
-
-        return Ok(node_to_replace);
-    }
-    // else return None
-    Err("node not found 😱")
 }
 
 fn walk_and_insert(node: &mut Node<i32>, item: i32) {
