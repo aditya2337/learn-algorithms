601bb29d8f40c3ae8d30ecf2422b5d1a0ce6ebb8
Aditya <androiditya@gmail.com>
2023-03-26 10:12:38 +0200

    Iterative matrix_multiply fixed

--- src/matrix_multiply.rs
+++ src/matrix_multiply.rs
@@ -1,12 +1,24 @@
 pub fn matrix_multiply(a: &Vec<Vec<i32>>, b: &Vec<Vec<i32>>) -> Vec<Vec<i32>> {
-    let a_row_length = a.len();
+    if a.len() == 0 && b.len() == 0 {
+        return vec![];
+    }
+    let a_col_length = a[0].len();
+    let b_row_length = b.len();
+
+
+
+    if a_col_length != b_row_length {
+        panic!("assertion failed");
+    }
+
     let b_col_length = b[0].len();
+    let a_row_length = a.len();
 
     let mut res = vec![vec![0; b_col_length]; a_row_length];
 
     for i in 0..a_row_length {
         for j in 0..b_col_length {
-            for k in 0..b.len() {
+            for k in 0..a_col_length {
                 res[i][j] += a[i][k] * b[k][j];
             }
         }
@@ -21,9 +33,9 @@ mod tests {
 
     #[test]
     fn test_matrix_multiply() {
-        let a = vec![vec![1, 2], vec![3, 4]];
-        let b = vec![vec![5, 6], vec![7, 8]];
-        let expected = vec![vec![19, 22], vec![43, 50]];
+        let a = vec![vec![1, 2, 3]];
+        let b = vec![vec![5, 6], vec![7, 8], vec![4, 5]];
+        let expected = vec![vec![31, 37]];
         assert_eq!(matrix_multiply(&a, &b), expected);
     }
 
