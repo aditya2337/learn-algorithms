50a5ef0c88d27eca98df7cddd822329679f0143d
Aditya <androiditya@gmail.com>
2023-04-29 06:37:51 +0200

    chore(avl-tree): start with test

--- src/binary_tree/AVL_tree.rs
+++ src/binary_tree/AVL_tree.rs
@@ -1,5 +1,6 @@
 type Link = Option<Box<Node>>;
 
+#[derive(Debug)]
 struct Node {
     val: i32,
     left_child: Link,
@@ -22,11 +23,16 @@ impl Node {
     }
 }
 
+#[derive(Debug)]
 struct AVLTree {
     root: Link,
 }
 
 impl AVLTree {
+    pub fn new() -> Self {
+        Self { root: None }
+    }
+
     pub fn insert(&mut self, val: i32) {
         let new_node = Node::new(val);
 
@@ -55,3 +61,19 @@ fn walk_and_insert(node: &mut Node, new_node: Node) {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::AVLTree;
+
+    #[test]
+    fn test_avl() {
+        let mut tree = AVLTree::new();
+        tree.insert(2);
+        tree.insert(1);
+        tree.insert(3);
+
+        println!("tree -------> {:?}", tree);
+    }
+}
+
