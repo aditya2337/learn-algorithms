3801f8e328422d8730826f420568f7f5ed887c4c
Aditya <androiditya@gmail.com>
2023-04-29 13:08:03 +0200

    chore(avl-tree): add insert logic to Avl node

--- src/binary_tree/AVL_tree.rs
+++ src/binary_tree/AVL_tree.rs
@@ -1,25 +1,48 @@
-type Link = Option<Box<Node>>;
+type Link = Option<Box<AVLNode>>;
 
 #[derive(Debug)]
-struct Node {
+struct AVLNode {
     val: i32,
     left_child: Link,
     right_child: Link,
-    balance_factor: i8,
+    height: usize,
 }
 
-impl Node {
+impl AVLNode {
     pub fn new(val: i32) -> Self {
         Self {
             val,
             left_child: None,
             right_child: None,
-            balance_factor: 0,
+            height: 1,
         }
     }
 
-    pub fn is_unbalanced(&self) -> bool {
-        self.balance_factor < -1 || self.balance_factor > 1
+    fn height(node: &Link) -> usize {
+        match node {
+            Some(node) => node.height,
+            None => 0,
+        }
+    }
+
+    fn get_left_height(&self) -> usize {
+        Self::height(&self.left_child)
+    }
+
+    fn get_right_height(&self) -> usize {
+        Self::height(&self.right_child)
+    }
+
+    pub fn balance_factor(&self) -> i8 {
+        (self.right_child.as_ref().unwrap().get_left_height()
+            - self.left_child.as_ref().unwrap().get_right_height())
+        .try_into()
+        .unwrap_or(0)
+    }
+
+    pub fn insert(&mut self, val: i32) {
+        let new_node = Self::new(val);
+        walk_and_insert(self, new_node);
     }
 }
 
@@ -34,17 +57,17 @@ impl AVLTree {
     }
 
     pub fn insert(&mut self, val: i32) {
-        let new_node = Node::new(val);
+        let new_node = AVLNode::new(val);
 
         if self.root.is_none() {
             self.root = Some(Box::new(new_node));
         } else {
-            walk_and_insert(self.root.as_mut().unwrap(), new_node);
+            self.root.as_mut().unwrap().insert(val);
         }
     }
 }
 
-fn walk_and_insert(node: &mut Node, new_node: Node) {
+fn walk_and_insert(node: &mut AVLNode, new_node: AVLNode) {
     if node.val > new_node.val {
         if node.left_child.is_none() {
             node.left_child = Some(Box::new(new_node));
@@ -76,4 +99,3 @@ mod tests {
         println!("tree -------> {:?}", tree);
     }
 }
-
