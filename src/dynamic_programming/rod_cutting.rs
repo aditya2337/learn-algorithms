4043afcb9393ca23881fd51813eac74f3a8a7000
Aditya <androiditya@gmail.com>
2023-05-12 21:41:51 +0200

    chore: Fixed the recursive rod cuttong prob

--- src/dynamic_programming/rod_cutting.rs
+++ src/dynamic_programming/rod_cutting.rs
@@ -1,16 +1,15 @@
-use std::{u8::MAX, cmp};
+use std::cmp;
 
 const PRICES: [u8; 10] = [1, 5, 8, 9, 10, 17, 17, 20, 24, 30];
 
-pub fn rod_cut_recursively(idx: usize) -> u8  {
+pub fn rod_cut_recursively(idx: usize) -> u8 {
     if idx == 0 {
         return 0;
     }
-    let mut max = 0;
+    let mut max = u8::MIN;
 
-
-    for i in 0..idx {
-        max = cmp::max(PRICES[i], rod_cut_recursively(idx - 1))
+    for i in 1..idx {
+        max = cmp::max(max, PRICES[i] + rod_cut_recursively(idx - i - 1))
     }
 
     return max;
@@ -22,7 +21,7 @@ mod tests {
 
     #[test]
     fn test_rod_cut() {
-        let max = rod_cut_recursively(8);
+        let max = rod_cut_recursively(4);
 
         assert_eq!(max, 10);
     }
